fa7d130ba79e7eb3f704acf0510783420b5fb2c4
Ilya Bulatnikov <61374383+panapey@users.noreply.github.com>
2023-11-27 15:09:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import re
 
 import chardet
 import pandas as pd
+import pymysql
+
+# Ваши данные для подключения к базе данных
+db_config = {
+    'host': 'HOST',
+    'port': PORT,
+    'user': 'USER',
+    'password': 'PASSWORD',
+    'db': 'DB',
+}
 
 
 def format_address(address):
@@ -27,7 +37,9 @@ def format_address(address):
         # Заменяем "пр-кт." на "пр-кт."
         address = re.sub(r"пр-кт\.", "пр-кт.", address)
         # Заменяем "проезд." на "проезд."
-        address = re.sub(r"проезд\.", "проезд.", address)
+        address = re.sub(r"проезд\.", "проезд", address)
+        # Заменяем "аллея." на "аллея"
+        address = re.sub(r"аллея\.", "аллея", address)
         # Заменяем "к." на "к"
         address = re.sub(r"к\.", "к", address)
         # Удаляем все лишние пробелы
@@ -38,26 +50,52 @@ def format_address(address):
         address = re.sub(r", ", " ", address)
         # Приводим адрес к шаблону "г. Город, д. Деревня, ул. Улица, д. ДомКорпус"
         address = re.sub(
-            r"(г\.\s*)(\w+)(,\s*б-р\.\s*|,\s*д\.\s*|,\s*ул\.\s*)?(\w+)?(,\s*д\.\s*)(\d+\w*)",
+            r"(г\.\s*)(\w+\\,)(,\s*б-р\.\s*|,\s*д\.\s*|,\s*ул\.\s*)?(\w+)?(,\s*д\.\s*)(\d+\w*)",
             r"\1\2\3\4\5\6", address)
-        # Заменяем второе упоминание "д." на пустую строку, если оно следует сразу после первого упоминания "д."
-        address = re.sub(r"(д\.\s*\d+\w*),\s*д\.", r"\1,", address)
+        # Удаляем "д." если за ним следуют цифры
+        address = re.sub(r"д\.\s*(\d+)", r"\1", address)
     except Exception as e:
         address = ""
     return address
 
 
-rawdata = open('adresses.csv', 'rb').read()
+rawdata = open('addresses.xlsx', 'rb').read()
 result = chardet.detect(rawdata)
 encoding = result['encoding']
 
 # Считываем CSV-файл
-df = pd.read_csv('adresses.csv', encoding=encoding, engine='python', sep=';')
+df = pd.read_excel('addresses.xlsx')
 print(df.columns)
 
 # Применяем функцию format_address к каждому адресу в столбце 'Address'
 df['Address'] = df['Address'].apply(format_address)
 
-# Сохраняем результат в новый CSV-файл
+# Создаем соединение с базой данных MySQL
+conn = pymysql.connect(host=db_config['host'], user=db_config['user'], password=db_config['password'],
+                       db=db_config['db'])
 
-df.to_excel('formated.xlsx')
+# Создаем курсор для выполнения SQL-запросов
+cursor = conn.cursor()
+
+# Создаем новый столбец в DataFrame для хранения результатов запроса
+df['QueryResults'] = ''
+
+# Проходим по каждому адресу в DataFrame
+for index, row in df.iterrows():
+    # Создаем SQL-запрос с текущим адресом
+    query = f"SELECT GROUP_NAME FROM `groups` g WHERE g.GROUP_NAME LIKE '%{row['Address']}%'"
+
+    # Выполняем SQL-запрос
+    cursor.execute(query)
+
+    # Получаем все строки из результата запроса
+    rows = cursor.fetchall()
+
+    # Записываем результаты запроса в новый столбец
+    df.at[index, 'QueryResults'] = str(rows)
+
+# Закрываем соединение с базой данных
+conn.close()
+
+# Сохраняем результат в новый CSV-файл
+df.to_excel('./addresses.xlsx')
